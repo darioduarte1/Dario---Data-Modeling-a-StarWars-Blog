e63ea822c413b2423f673d8ba4f1d7a80fa7a688
darioduarte1 <darioduarte1991@gmail.com>
2024-07-26 08:11:27 +0000

    Build a StarWars REST API

--- src/models.py
+++ src/models.py
@@ -7,23 +7,52 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=True)
+    first_name = Column(String(250), nullable=True)
+    last_name = Column(String(250), nullable=True)
+    email = Column(String(250), nullable=True)
+    password = Column(String(250), nullable=True)
+    favorite = relationship("Favorites", backref="user")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Characters(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=True)
+    race = Column(String(250), nullable=True)
+    height = Column(String(250), nullable=True)
+    eyes_color = Column(String(250), nullable=True)
+    favorite = relationship("Favorites", backref="characters")
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=True)
+    local = Column(String(250), nullable=True)
+    dimension = Column(String(250), nullable=True)
+    color = Column(String(250), nullable=True)
+    favorite = relationship("Favorites", backref="planets")
+
+class Spaceships(Base):
+    __tablename__ = 'spaceships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=True)
+    color = Column(String(250), nullable=True)
+    guns = Column(String(250), nullable=True)
+    velocity = Column(String(250), nullable=True)
+    favorite = relationship("Favorites", backref="spaceships")
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_fk = Column(Integer, ForeignKey("user.id"))
+    characters_fk = Column(Integer, ForeignKey("characters.id"))
+    planet_fk = Column(Integer, ForeignKey("planets.id"))
+    spaceships_fk = Column(Integer, ForeignKey("spaceships.id"))
+    
+
 
     def to_dict(self):
         return {}
